2645107a3502f5495fe1c3ed709bb98758fd6935
TrX9 <xxtmxx9@gmail.com>
2024-07-11 12:38:26 +0300

    Add project files.

---
+++ SimpleWpfApp/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace SimpleWpfApp
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Person _person;
+
+        public MainViewModel()
+        {
+            _person = new Person();
+            UpdateCommand = new RelayCommand(UpdatePerson);
+        }
+
+        public Person Person
+        {
+            get { return _person; }
+            set
+            {
+                _person = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand UpdateCommand { get; }
+
+        private void UpdatePerson(object parameter)
+        {
+            Person.FirstName = "John";
+            Person.LastName = "Doe";
+            Person.Age = 30;
+            OnPropertyChanged(nameof(Person));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter);
+
+        public void Execute(object parameter) => _execute(parameter);
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
